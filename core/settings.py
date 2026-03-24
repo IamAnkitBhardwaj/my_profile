feaf953e8679e711f739f61e3542e53335e4f81a
IamAnkitBhardwaj <ankitk221706@gmail.com>
2026-03-24 09:20:33 +0530

    image fix

--- core/settings.py
+++ core/settings.py
@@ -102,9 +102,7 @@ STATICFILES_DIRS = [
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# MEDIA FILES
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # DEFAULT PRIMARY KEY
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
